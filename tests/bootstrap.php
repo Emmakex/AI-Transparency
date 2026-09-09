9c4ec2a58c10e5087e7336c585c9697ad7af98fe
Edu <133777880+Emmakex@users.noreply.github.com>
2026-09-09 13:29:42 +0200

    test: load WordPress-style autoloader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * PHPUnit bootstrap.
+ * PHPUnit bootstrap for pure PHP domain tests.
  *
  * @package KairosethAITransparency
  */
 
-require_once dirname( __DIR__ ) . '/src/Autoloader.php';
+require_once dirname( __DIR__ ) . '/src/class-autoloader.php';
 
 \Kairoseth\AITransparency\Autoloader::register();

35a2ddf97fdd38f9d27a1d4b1c44d3e0a0c3dc1e
Edu <133777880+Emmakex@users.noreply.github.com>
2026-09-09 13:22:43 +0200

    test: add unit test bootstrap

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * PHPUnit bootstrap.
+ *
+ * @package KairosethAITransparency
+ */
+
+require_once dirname( __DIR__ ) . '/src/Autoloader.php';
+
+\Kairoseth\AITransparency\Autoloader::register();
